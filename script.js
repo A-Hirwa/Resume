57d4238019e686a5d7009976ab60ba606fefbed1
A-Hirwa <a.hirwa1@alustudent.com>
2025-02-20 15:15:27 +0200

    adding comments

--- script.js
+++ script.js
@@ -1,33 +1,10 @@
+// function to show side navigation
 function shownav() {
     const nav = document.querySelector(".side-nav");
     nav.style.display = "flex"
 }
+// function to hide side navigation
 function hidenav() {
     const nav = document.querySelector(".side-nav");
     nav.style.display = "none"
-}
-
-function skills(){
-    const skill = document.querySelector("#skills")
-    const education = document.querySelector("#education")
-    const experience= document.querySelector("#experience")
-    skill.style.display = "block"
-    education.style.display = "none"
-    experience.style.display = "none"
-}
-function education(){
-    const skill = document.querySelector("#skills")
-    const education = document.querySelector("#education")
-    const experience= document.querySelector("#experience")
-    skill.style.display = "none"
-    education.style.display = "block"
-    experience.style.display = "none"
-}
-function experience(){
-    const skill = document.querySelector("#skills")
-    const education = document.querySelector("#education")
-    const experience= document.querySelector("#experience")
-    skill.style.display = "none"
-    education.style.display = "none"
-    experience.style.display = "block"
 }
